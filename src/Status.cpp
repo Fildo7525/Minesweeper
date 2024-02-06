3de17934d573584429df80dfa3f4cb850cbb9ee0
Filip Lobpreis <f.lobpreis@gmail.com>
2024-02-06 10:02:37 +0100

    status: setup difficulty setting
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- src/Status.cpp
+++ src/Status.cpp
@@ -4,16 +4,30 @@
 Status::Status(std::shared_ptr<Board>  &board)
 	: Layer("Status")
 	, m_board(board)
+	, m_difficulty(0)
 {
 
 }
 
 void Status::render()
 {
-	ImGui::Begin("Game Status");
+	ImGui::Begin("Game Status", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar);
 
 	ImGui::Text("Mines: %d / %d", m_board->numberOfFlags(), m_board->totalNumberOfMines());
-	// ImGui::Text("Elapsed time: %d", m_board->elapsedTime());
+	auto minutes = m_board->elapsedTime() / 60;
+	ImGui::Text("Elapsed time: %2ld:%ld", minutes, m_board->elapsedTime()%60);
+
+	{
+		if (ImGui::RadioButton("Easy", &m_difficulty, 0)) {
+			m_board->setDifficulty(0);
+		}
+		if (ImGui::RadioButton("Medium", &m_difficulty, 1)) {
+			m_board->setDifficulty(1);
+		}
+		if (ImGui::RadioButton("Hard", &m_difficulty, 2)) {
+			m_board->setDifficulty(2);
+		}
+	}
 
 	ImGui::End();
 }
--- src/Status.h
+++ src/Status.h
@@ -8,7 +8,9 @@ class Status
 public:
 	explicit Status(std::shared_ptr<Board> &board);
 	void render() override;
+	int difficulty() const { return m_difficulty; }
 
 private:
 	std::shared_ptr<Board> m_board;
+	int m_difficulty;
 };
