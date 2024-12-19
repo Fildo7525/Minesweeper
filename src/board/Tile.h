87713039408b95096a2789ed3e4f8eb43c8efe0a
Filip Lobpreis <f.lobpreis@gmail.com>
2024-12-19 20:10:10 +0100

    tile: add documentation
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- src/board/Tile.h
+++ src/board/Tile.h
@@ -11,7 +11,6 @@
 struct Pose {
 	int x;
 	int y;
-
 };
 
 bool operator==(const Pose &lhs, const Pose &rhs);
@@ -33,16 +32,61 @@ class Tile
 public:
 	explicit Tile(Icon::Ocupant ocupant, Pose position, bool clicked = false);
 
+	/**
+	 * @brief Check if the tile belongs to the given player.
+	 *
+	 * @see Icon::Ocupant The enum representing the players.
+	 *
+	 * @param ocupant The ocupant to check the ownership.
+	 * @return True if the tile belongs to the given player, false otherwise.
+	 */
 	bool belongsTo(const Icon::Ocupant &ocupant) const;
 
+	/**
+	 * @brief Setup the tile with a given ocupant.
+	 *
+	 * @param ocupant The ocupant to set the tile to.
+	 * @return The reference to the tile object.
+	 */
 	Tile &setOcupant(const Icon::Ocupant &ocupant) { m_ocupant = ocupant; return *this; }
 
+	/**
+	 * Returns the position of the tile.
+	 */
 	Pose position() const { return m_position; }
-	bool clicked() const { return m_clicked; }
-	Tile &click(bool c = true); //{}
 
+	/**
+	 * True if the tile was clicked, false otherwise.
+	 */
+	bool clicked() const { return m_clicked; }
+
+	/**
+	 * @brief Click/Unclick the tile.
+	 *
+	 * @param c True if the tile was clicked, false means the tile was unclicked.
+	 * @return The reference to the tile object.
+	 */
+	Tile &click(bool c = true);
+
+	/**
+	 * @brief Render the tile.
+	 *
+	 * The color of the tile is determined by clicked state and the ocupant.
+	 *
+	 * @return ImColor representation of the tile in the current state.
+	 */
 	ImColor color() const;
+
+	/**
+	 * Get the ocupant of the tile.
+	 */
 	Icon::Ocupant ocupant() const;
+
+	/**
+	 * Get the icon of the tile.
+	 *
+	 * @see Icon::Ocupant The enum representation of the tile state.
+	 */
 	std::shared_ptr<Icon> icon() const { return m_icons[(int)m_ocupant]; }
 
 private:
