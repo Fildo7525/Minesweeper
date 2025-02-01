450f1120536bc73e4bf25994eb06bf5e9c24431e
Filip Lobpreis <f.lobpreis@gmail.com>
2025-02-01 21:08:49 +0100

    pqueue: add comments
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- src/board/records/PQueue.h
+++ src/board/records/PQueue.h
@@ -5,6 +5,16 @@
 #include <vector>
 #include <functional>
 
+/**
+ * @brief Priority queue with dynamic sorting.
+ *
+ * The PQueue class is a priority queue based on std::vector<T> that sorts the elements
+ * in the container using the given sorter. The main difference from std::priority_queue<T>
+ * is that this class's sorter can be changed at runtime. The default sorter is std::greater<T>
+ * which sorts the elements in descending order.
+ *
+ * @tparam T Type of the elements in the container.
+ */
 template <typename T>
 class PQueue : public std::vector<T>
 {
@@ -17,6 +27,13 @@ public:
 
 	~PQueue() = default;
 
+	/**
+	 * @brief Set the dynamic sorter for the priority queue.
+	 *
+	 * @param sorter Sorter function that takes two elements of type T and returns true if the first.
+	 * @param invoke Flag to invoke the sorter immediately after setting it.
+	 * @return PQueue& Reference to the priority queue.
+	 */
 	PQueue &setSorter(std::function<bool(const T &, const T &)> sorter, bool invoke = true)
 	{
 		m_sorter = sorter;
@@ -27,27 +44,48 @@ public:
 		return *this;
 	}
 
+	/**
+	 * @brief Sort the elements in the container.
+	 */
 	void sort()
 	{
 		std::sort(std::execution::par_unseq, this->begin(), this->end(), m_sorter);
 	}
 
+	/**
+	 * @brief Push an element to the priority queue.
+	 *
+	 * @param value Element to push to the priority queue.
+	 */
 	void push(const T &value)
 	{
 		this->push_back(value);
 		sort();
 	}
 
+	/**
+	 * @brief Pop the top element from the priority queue.
+	 */
 	void pop()
 	{
 		this->pop_back();
 	}
 
+	/**
+	 * @brief Get the top element from the priority queue.
+	 *
+	 * @return T& Reference to the top element.
+	 */
 	T &top()
 	{
 		return this->front();
 	}
 
+	/**
+	 * @brief Get the top element from the priority queue.
+	 *
+	 * @return const T& Reference to the top element.
+	 */
 	const T &top() const
 	{
 		return this->front();
